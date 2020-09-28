a1dbc4008dd7e8f29a38109a0f1a9289dbedb55a
emma.ou <ouwenxia1994@foxmail.com>
2020-09-28 18:10:27 +0800

    add: 15-二分查找

---
+++ 数据结构与算法之美/15-二分查找/binary.js
@@ -0,0 +1,26 @@
+// 二分查找
+
+// 数组必须有序， 不存在重复
+
+const binaryFind = (sortedArr, target) => {
+  if (sortedArr.length === 0) return -1;
+  let low = 0;
+  let high = sortedArr.length - 1;
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    if (target === sortedArr[mid]) {
+      return mid;
+    } else if (target < sortedArr[mid]) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return -1;
+};
+
+const arr = [1, 4, 5, 6, 7, 8, 10, 11, 23, 42, 44, 54, 56, 77, 102];
+console.log(binaryFind(arr, 44));
+console.log(binaryFind(arr, 1));
+console.log(binaryFind(arr, 102));
+console.log(binaryFind(arr, 1111));
